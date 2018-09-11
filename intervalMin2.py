5dc6298e74dcbc91bf92bef1755f3f5e46bbc78e
ealmenzar <marina.estal@gmail.com>
2018-09-11 20:05:14 +0200

    Important intervalMin2 corrections

--- intervalMin2.py
+++ intervalMin2.py
@@ -2,8 +2,9 @@ import random
 import numpy as np
 from redMethRotCurveFitting import phi, varphiLimInf, varphiLim0
 import time
+import matplotlib.pyplot as plt
 
-tol = 10**-6
+tol = 10**-2
 maxiter = 0
 direction = -1
 intervalinf = -3
@@ -14,7 +15,8 @@ lastint = 0.0
 dir = []
 stop = False
 i = 0.0
-
+X = []
+Y = []
 random.seed(1)
 
 #eval = (phi(10**(intervalinf + np.array([-0.2, -0.1, 0.0, 0.1, 0.2]))) - varphiLim0) / varphiLim0
@@ -23,22 +25,25 @@ random.seed(1)
 
 while maxiter < 100 and direction != 0 and k < 50:
     maxiter += 1
-    eval = abs(phi(10**(intervalinf + np.array([-0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.10, 0.15, 0.2]))) - varphiLim0) / varphiLim0
+    varphi = phi(10**(intervalinf + np.array([-0.2, -0.1, 0.0, 0.1, 0.2])))
+    eval = abs(varphi - varphiLim0) / varphiLim0
+    X.append(10**intervalinf + np.array([-0.2, -0.1, 0.0, 0.1, 0.2]))
+    Y.append(varphi)
     #print("eval[2] = ", eval[2])
     #print("eval[3] = ", eval[3])
     #print("eval[4] = ", eval[4])
     #print("sum(eval[2:5]) = ", sum(eval[2:5]))
     #print("sum(eval[0:3]) = ", sum(eval[0:3]))
-    # [2:5] [0,3]
+    # [2:5] [0,2]
     # [5:10] [0:5]
     #test1 = sum(eval[2:5]) < tol
     #test2 = sum(eval[0:3]) < tol
     test1 = True                    # En vez de considerar la suma de los elementos de eval, se considera
     test2 = True                    # cada uno de forma independiente
-    for e in eval[5:10]:
+    for e in eval[2:5]:
         if e > tol:
             test1 = False
-    for e in eval[0:5]:
+    for e in eval[0:2]:
         if e > tol:
             test2 = False
 
@@ -80,10 +85,11 @@ while maxiter < 100 and direction != 0 and k < 50:
     else:
         k = 0
     lastint = 10**intervalinf
+    dir.append(direction)
 
 print("maxiter = ", maxiter)
 print("k = ", k)
-#print("dir = ", dir)
+print("dir = ", dir)
 print("direction = ", direction)
 direction = 1
 maxiter = 0
@@ -93,23 +99,29 @@ dir.clear()
 stop = False
 i = 0.0
 
+
 while maxiter < 100 and direction != 0 and k < 50:  # Nueva condición de parada --> relacionar con el límite
     maxiter += 1
     # [-0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.10, 0.15, 0.2]
-    eval = (phi(10**(intervalsup + np.array([-0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.10, 0.15, 0.2]))) - varphiLimInf) / varphiLimInf
-    # [2:5] [0,3]  corregir
+    varphi = phi(10**(intervalsup + np.array([-0.2, -0.1, 0.0, 0.1, 0.2])))
+    eval = abs(varphi - varphiLimInf) / varphiLimInf    ##  CORREGIDO
+    X.append(10**(intervalsup + np.array([-0.2, -0.1, 0.0, 0.1, 0.2])))
+    Y.append(varphi)
+    # [3:5] [0,3]
     # [5:10] [0:5]
     #test1 = sum(eval[0:3]) < tol
     #test2 = sum(eval[2:5]) < tol
     test1 = True        # En vez de considerar la suma de los elementos de eval, se considera
     test2 = True        # cada uno de forma independiente
-    for e in eval[5:10]:
+    for e in eval[3:5]:
         if e > tol:
             test2 = False
-    for e in eval[0:5]:
+    for e in eval[0:3]:
         if e > tol:
             test1 = False
     if test1 == 0 and test2 == 1:
+        print("maxiter = ", maxiter)
+        print("diferencia = ", abs(phi(np.asarray([10**intervalsup])) - varphiLimInf))
         stop = True         # Podemos convertir stop en un contador, en lugar de un booleano, pera
         direction = 1       # establecer un patrón más específico --> agrandar el boleano: 0.3 - 0.6
         i = intervalsup
@@ -124,6 +136,7 @@ while maxiter < 100 and direction != 0 and k < 50:  # Nueva condición de parada
         if stop:
             direction = 0
             intervalsup = i
+            diferencia = abs(phi(np.asarray([10**intervalsup])) - varphiLimInf)
         else:
             direction = -1
             intervalsup = intervalsup + random.uniform(0.2, 0.3) * direction
@@ -133,6 +146,8 @@ while maxiter < 100 and direction != 0 and k < 50:  # Nueva condición de parada
         direction = 1
         intervalsup = intervalsup + random.uniform(0.2, 0.3) * direction
     if abs(10**intervalsup - lastint) < tol:
+        #if k >= 10 and abs(phi(np.asarray(10**intervalsup)) - varphiLimInf):
+         #   k = 0
         k += 1
         print("intervalsup = ", intervalsup)
         #dir.append(direction)
@@ -141,9 +156,18 @@ while maxiter < 100 and direction != 0 and k < 50:  # Nueva condición de parada
     lastint = 10**intervalinf
 
 print("k = ", k)
+print("diferencia = ", diferencia)
 #print("dir = ", dir)
 print("maxiter = ", maxiter)
 print("direction = ", direction)
+Xa = np.asarray(X)
+Ya = np.asarray(Y)
+plt.scatter(X, Y)
+plt.hlines(varphiLimInf, 0, 1500)
+plt.hlines(varphiLim0, 0, 100)
+plt.vlines(10**intervalsup, 0, 50)
+plt.vlines(10**intervalinf, 50, 100)
+plt.show()
 
 if intervalinf > intervalsup:
     print("The interval selection module has not worked properly")
