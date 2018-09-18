ff20ffe1931529d5a9ca2bbd26cb5e2c0118f1d5
ealmenzar <marina.estal@gmail.com>
2018-09-18 12:37:25 +0200

    Some corrections in varphi minimization

--- varphiMinim.py
+++ varphiMinim.py
@@ -2,7 +2,8 @@ import random
 import numpy as np
 from commonFunctions import phi
 from redMethRotCurveFitting import intervalinf, intervalsup, galaxdata
-'''
+random.seed(1)
+
 def getIMD(intizq, intder, galaxdata):
     m = (intder + intizq) / 2
     M = phi(np.array([m]), 'ISO', galaxdata)
@@ -11,110 +12,56 @@ def getIMD(intizq, intder, galaxdata):
     d = random.uniform(m, intder)
     D = phi(np.array([d]), 'ISO', galaxdata)
 
-    return {'I': I, 'M': M, 'D': D}
+    return {'I': I, 'i': i, 'M': M, 'm': m, 'D': D, 'd': d}
 
-def forkRoutine(subint1, subint2, bestphi, tol, galaxdata):
-    bestphi1 = subintervalRoutine(subint1, 0, bestphi, tol, False, galaxdata)
-    bestphi2 = subintervalRoutine(subint2, 0, bestphi, tol, False, galaxdata)
-    bestphi = min(bestphi1, bestphi2)
+def varphiMin(intervalinf, intervalsup, galaxdata):
+    tol = 10**-8
+    subint = np.asarray(np.logspace(np.log10(intervalinf), np.log10(intervalsup), 8))
+    bestphi = 10**4
 
-    return bestphi
-
-def subintervalRoutine(subint, i, bestphi, tol, fork, galaxdata):
-    print("SUBINT = ", i)
-    intizq = subint[i]
-    intder = subint[i + 1]
-    M = 1
-    lastM = 0
-    minphi = 10 ** 4
-    k = 0
-    while abs(M - lastM) > tol:
-        print(k)
-        lastM = M
-        print("intizq = ", intizq, "; intder = ", intder)
-        IMD = getIMD(intizq, intder, galaxdata)
-        M = IMD['M']
-        print("M = ", M)
-        I = IMD['I']
-        print("I = ", I)
-        D = IMD['D']
-        print("D = ", D)
-        if I < M < D:
-            intder = m
-        elif I > M > D:
-            intizq = m
-        else:
-            if fork:
-                intizqf = intizq
-                intderf = intder
-                forkRoutine([intizqf, m], [m, intderf])
+    for s in np.arange(len(subint) - 1):
+        print("SUBINT = ", s)
+        print("len(SUBINT) = ", len(subint))
+        print("subint = ", type(subint))
+        intizq = subint[s]
+        intder = subint[s + 1]
+        M = 1
+        lastM = 0
+        minphi = 10**4
+        k = 0
+        fork = 0
+        while abs(M - lastM) > tol:
+            print(k)
+            lastM = M
+            print("intizq = ", intizq, "; intder = ", intder)
+            IMD = getIMD(intizq, intder, galaxdata)
+            m = IMD['m']
+            M = IMD['M']
+            print("M = ", M)
+            i = IMD['i']
+            I = IMD['I']
+            print("I = ", I)
+            d = IMD['d']
+            D = IMD['D']
+            print("D = ", D)
+            if I < M < D:
+                intder = m
+            elif I > M > D:
+                intizq = m
             else:
-                if I < D:
-                    intder = m
-                else:
-                    intizq = m
-        if min(IMD) < minphi:
-            minphi = min(IMD)
-        k += 1
-        # print("intervalo = [", intizq, ", ", intder, "]")
-    if minphi < bestphi:
-        bestphi = minphi
-
-    return bestphi
-'''
-#def varphiMin(intervalinf, intervalsup, galaxdata):
-random.seed(1)
-tol = 10**-2
-subint = np.asarray(np.logspace(np.log10(intervalinf), np.log10(intervalsup), 8))
-bestphi = 10**4
-
-for s in np.arange(len(subint) - 1):
-    print("SUBINT = ", s)
-    print("len(SUBINT) = ", len(subint))
-    print("subint = ", type(subint))
-    intizq = subint[s]
-    intder = subint[s + 1]
-    M = 1
-    lastM = 0
-    minphi = 10**4
-    k = 0
-    while abs(M - lastM) > tol:
-        print(k)
-        lastM = M
-        print("intizq = ", intizq, "; intder = ", intder)
-        m = (intder + intizq) / 2
-        print("m = ", m)
-        M = phi(np.array([m]), 'ISO', galaxdata)
-        print("M = ", M)
-        i = random.uniform(intizq, m)
-        print("i = ", i)
-        I = phi(np.array([i]), 'ISO', galaxdata)
-        print("I = ", I)
-        d = random.uniform(m, intder)
-        print("d = ", d)
-        D = phi(np.array([d]), 'ISO', galaxdata)
-        print("D = ", D)
-        if I < M < D:
-            intder = m
-        elif I > M > D:
-            intizq = m
-        else:
-            intder = m
-            print("SUBINT LAST = ", subint)
-            print("s+1 = ", s+1)
-            subint = np.insert(subint, s+1, m)
-            print("SUBINT NEW = ", subint)
-        if M < minphi:
-            minphi = M
-        k += 1
-        #print("intervalo = [", intizq, ", ", intder, "]")
-    if minphi < bestphi:
-        bestphi = minphi
-
-    print("BESTPHI = ", bestphi)
-
-
-
-
-
-#return 0
+                fork += 1
+                intder = m
+                print("SUBINT LAST = ", subint)
+                print("s+1 = ", s+1)
+                subint = np.insert(subint, s+1, m)
+                print("SUBINT NEW = ", subint)
+            print("min = ", min([I, M, D]))
+            if min([I, M, D]) < minphi:
+                minphi = min([I, M, D])
+            print("minphi = ", minphi)
+            k += 1
+            #print("intervalo = [", intizq, ", ", intder, "]")
+        if minphi < bestphi:
+            bestphi = minphi
+        print("FORK = ", fork)
+        print("BESTPHI = ", bestphi)
