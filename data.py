5b8b63702a50f156231251d4c9364d225d013315
ealmenzar <marina.estal@gmail.com>
2018-11-22 21:30:38 +0100

    Data changes

--- data.py
+++ data.py
@@ -2,16 +2,20 @@
 from scipy.io import arff
 import numpy as np
 
+## GALAXIES DATA READING AND CONSTANTS ##
+
 galaxlist = [
-            "DDO43", "DDO46", "DDO47", "DDO52", "DDO53", "DDO70", "DDO87", "DDO101", "DDO126",
-             "DDO133", "DDO154", "DDO168", "DDO210", "DDO216", "F564_v3", "haro29", "haro36", "ic10", "ic1613",
-             "NGC1569", "NGC2366", "NGC3738", "UGC8508"
+            "DDO43"
+    #, "DDO46", "DDO47"
+    #, "DDO52", "DDO53", "DDO70", "DDO87", "DDO101", "DDO126",
+     #        "DDO133", "DDO154", "DDO168", "DDO210", "DDO216", "F564_v3", "haro29", "haro36", "ic10", "ic1613",
+      #       "NGC1569", "NGC2366", "NGC3738", "UGC8508"
             ]
 galaxies = {}
 for i in galaxlist:
     fp = open("galaxies/"+i+".arff")
-    # data = arff.load(fp)              CON PYTHON 3.5
-    # data = np.array(data['data'])     CON PYTHON 3.5
+    # data = arff.load(fp)              # CON PYTHON 3.5
+    # data = np.array(data['data'])     # CON PYTHON 3.5
     dt, metadt = arff.loadarff(fp)      # CON PYTHON 3.7
     data = []                           # CON PYTHON 3.7
     for d in dt.tolist():               # CON PYTHON 3.7
@@ -24,5 +28,5 @@ for i in galaxlist:
         "vbary": np.sqrt(data[:, 4] ** 2 + data[:, 5] ** 2)
     }
     fp.close()
-nu = 2
+nu = 2  # SÓLO PARA ISO, BUR Y NFW (PARA EIN nu = 3)
 CteDim = 10000 / ((3.0856776 ** 2) * 4.51697)
--- proof.py
+++ proof.py
@@ -1,21 +1,6 @@
-from WeighProd import WeighProd
 import numpy as np
 
-v = np.array([0.1,0.1])
-m = np.array([[1,3],[2,4]])
-
-x1 = np.arange(3.0)
-x2 = np.arange(3.0)
-
-a = np.array([5,4])[np.newaxis]
-
-
-x = np.ones(5)
-print(x)
-print(x.T)
-y = np.atleast_2d(x)
-print(y)
-print(y.T)
-
-
-
+matr = np.asmatrix([[1,1,1,1], [2,2,2,2], [3,3,3,3]])
+print(type(matr))
+print(matr)
+print(len(matr))
