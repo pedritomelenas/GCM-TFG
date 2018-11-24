0e9fd63fdcde2b802de6a101df99efb3bd35cd0a
ealmenzar <marina.estal@gmail.com>
2018-11-24 20:51:44 +0100

    New 3D graphics for the report

--- galaxygraphic3D.py
+++ galaxygraphic3D.py
@@ -6,9 +6,9 @@ def generate3Dgraphic(i, minvarphiX, minvarphi, minrho, galaxdata):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ssurf = np.linspace(0.0015, 0.001, 50)
-    minorssurf = np.linspace(0.0015, 0.001, 7)
+    minorssurf = np.linspace(0.0015, 0.001, 8)
     rhosurf = np.linspace(0.01, 0.03, 50)
-    csurf = cf.chiquad(rhosurf, ssurf, galaxdata)
+    # csurf = cf.chiquad(rhosurf, ssurf, galaxdata) # Scatter data
     # surf = ax.scatter3D(ssurf, rhosurf, csurf)    # Scatter data
     Xsurf, Ysurf = np.meshgrid(ssurf, rhosurf)
     chisurf = np.zeros((len(Xsurf), len(Ysurf)))
@@ -16,16 +16,17 @@ def generate3Dgraphic(i, minvarphiX, minvarphi, minrho, galaxdata):
         for j in range(len(Ysurf)):
             chielem = cf.chiquad(np.array([Ysurf[k,j]]), np.array([Xsurf[k,j]]), galaxdata)
             chisurf[k][j] = chielem
-    surf = ax.plot_surface(Xsurf, Ysurf, chisurf, cmap="winter", alpha=0.4)
-    ax.plot(ssurf, cf.rho(ssurf, galaxdata), cf.chiquad(cf.rho(ssurf, galaxdata), ssurf, galaxdata), c='b')
-    ax.plot([minvarphiX] * 50, rhosurf, cf.chiquad(rhosurf, np.asarray([minvarphiX] * 50), galaxdata), c='b')
+    surf = ax.plot_surface(Xsurf, Ysurf, chisurf, cmap="winter", alpha=0.4)     # Superficie
+    # Función rho_0(s) para s in (0, infty) // varphi(s) es la proyección de esta rho_0(s) en el plano rho_0 = 0 #
+    ax.plot(ssurf, cf.rho(ssurf, galaxdata), cf.chiquad(cf.rho(ssurf, galaxdata), ssurf, galaxdata), c='r')
+    # Función chi^2(·, s)  para un s dado #
+    ax.plot([minvarphiX] * 50, rhosurf, cf.chiquad(rhosurf, np.asarray([minvarphiX] * 50), galaxdata), c='green')
+    ax.scatter3D(minvarphiX, minrho, minvarphi, c='black', linewidths=2)
     for x in minorssurf:
-        ax.plot([x] * 50, rhosurf, cf.chiquad(rhosurf, np.asarray([x] * 50), galaxdata), c='green', alpha=0.8)
+        ax.plot([x] * 50, rhosurf, cf.chiquad(rhosurf, np.asarray([x] * 50), galaxdata), c='green', alpha=0.4)
         convexmin = min(cf.chiquad(rhosurf, np.asarray([x] * 50), galaxdata))
-        ax.scatter3D(x, cf.rho(np.array([x]), galaxdata), convexmin, c='black')
-    ax.scatter3D(minvarphiX, minrho, minvarphi, c='black')
-    #ax.plot(minvarphiX, cf.rho(minvarphiX, galaxdata), cf.phi(ssurf, galaxdata), c='yellow')     # ERROR
-    ax.view_init(0, 100)
+        ax.scatter3D(x, cf.rho(np.array([x]), galaxdata), convexmin, c='red', linewidths=0.1)       # Funciones convexas
+    ax.view_init(0, 90)
     ax.yaxis.labelpad = 5
     ax.xaxis.labelpad = 10
     ax.set_xlabel('s')
@@ -33,4 +34,4 @@ def generate3Dgraphic(i, minvarphiX, minvarphi, minrho, galaxdata):
     ax.set_zlabel(r'$\chi^2(\rho_0,s)$')
     plt.title("Galaxia " + i + " con perfil " + galaxdata["profile"])
     plt.show()
-    #plt.savefig("galaxies/graphics/" + galaxdata["profile"] + '-' + i + '-3D_convexity0100.png')
+    #plt.savefig("galaxies/graphics/" + galaxdata["profile"] + '-' + i + '-3D_convexity0190.png')
