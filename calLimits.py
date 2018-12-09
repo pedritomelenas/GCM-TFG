0f8f8c2fa0d0ea24eb2042e0c6e9d4511fd13c82
ealmenzar <marina.estal@gmail.com>
2018-12-09 01:12:42 +0100

    Some comments

--- calLimits.py
+++ calLimits.py
@@ -13,10 +13,8 @@ import scipy.optimize as op
 def calLimits(galaxdata):
     ginf = cf.ginf(galaxdata["radii"], galaxdata["profile"])
     g0 = cf.g0(galaxdata["radii"], galaxdata["profile"])
-    # The solution to #34 --> #35 with vbary=0, which is an upper bound for the solutions to #34 --> #35
     XVbaryNullinf = (cf.WeighProd(galaxdata["vrot"], np.sqrt(ginf), galaxdata["weights"]) /
                      cf.WeighProd(ginf, galaxdata["vones"], galaxdata["weights"])) ** 2
-    # The solution to #40 is an upper bound for the solutions to #40
     XVbaryNull0 = (cf.WeighProd(galaxdata["vrot"], np.sqrt(g0), galaxdata["weights"]) /
                    cf.WeighProd(g0, galaxdata["vones"], galaxdata["weights"])) ** 2
 
@@ -30,24 +28,20 @@ def calLimits(galaxdata):
         j0 = -3
         while cf.eqVLim0(10 ** j0 * XVbaryNull0, g0, galaxdata) > 0:
             j0 -= 1
-        Xinf = op.brentq(cf.eqVLimInf, 10 ** jinf * XVbaryNullinf, XVbaryNullinf)  # Brent's Method (escalar)
-        X0 = op.brentq(cf.eqVLim0, 10 ** j0 * XVbaryNull0, XVbaryNull0)  # Brent's Method (escalar)
-
+        Xinf = op.brentq(cf.eqVLimInf, 10 ** jinf * XVbaryNullinf, XVbaryNullinf)   # Brent's Method
+        X0 = op.brentq(cf.eqVLim0, 10 ** j0 * XVbaryNull0, XVbaryNull0)             # Brent's Method
     else:
         if cf.eqVLimInf(0, ginf, galaxdata) >= 0:
             Xinf = 0
         else:
-            Xinf = op.brentq(cf.eqVLimInf, 0, XVbaryNullinf, (ginf, galaxdata))  # Solves equation --> #35
+            Xinf = op.brentq(cf.eqVLimInf, 0, XVbaryNullinf, (ginf, galaxdata))
         if cf.eqVLim0(0, g0, galaxdata) >= 0:
             X0 = 0
         else:
-            X0 = op.brentq(cf.eqVLim0, 0, XVbaryNull0, (g0, galaxdata))  # Solves equation #38 --> #40
+            X0 = op.brentq(cf.eqVLim0, 0, XVbaryNull0, (g0, galaxdata))
 
-    ## Calculation of the limit value by using Lemma and development #16 ##
     varphiLimInf = galaxdata["vv"] + galaxdata["vvbary"] + Xinf * cf.WeighProd(ginf, galaxdata["vones"], galaxdata["weights"])\
                    - 2 * cf.WeighProd(galaxdata["vrot"], np.sqrt(Xinf * ginf + (galaxdata["vbary"] ** 2)), galaxdata["weights"])
-
-    ## Calculation of the limit value by using Lemma and development #16 ##
     varphiLim0 = galaxdata["vv"] + galaxdata["vvbary"] + X0 * cf.WeighProd(g0, galaxdata["vones"], galaxdata["weights"]) \
                  - 2 * cf.WeighProd(galaxdata["vrot"], np.sqrt(X0 * g0 + (galaxdata["vbary"] ** 2)), galaxdata["weights"])
 
